1f83bc85b6e4c2f7c6e14a872a722bd89b3dcd11
jaykumar286 <jaykumarchavda2806@gmail.com>
2023-10-07 22:18:13 +0530

    Added the Message Queue!!

--- BookingService/src/controllers/booking-controller.js
+++ BookingService/src/controllers/booking-controller.js
@@ -1,35 +1,86 @@
-const {StatusCodes} = require("http-status-codes");
-
-const {BookingService} = require("../services/index");
+const { StatusCodes } = require("http-status-codes");
+const { BookingService } = require("../services/index");
+const { createChannel, publishMessage } = require("../utils/message-queue");
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
 
 const bookingService = new BookingService();
 
-const create = async(req,res) => {
+class BookingController {
+
+  async sendMessageQueue(req, res) {
     try {
-
-        const response = await bookingService.createBooking(req.body);
-        return res.status(StatusCodes.OK).json(
-            {
-                data:response,
-                message:"Successfully Booking",
-                isSucess:true,
-                err:{}
-            }
-        );
+      const channel = await createChannel();
+      const payload = { 
+        data:{
+            subject:"this a subject2",
+            content:"this is a content2",
+            recepientEmail:"19BCE038@nirmauni.ac.in",
+            notificationTime:"2021-07-06 09:49:00.000000"
+        },
+        service:"CREATE_TICKET"
+      };
+      publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
+      return res.status(StatusCodes.OK).json({
+        data: payload.data,
+        message: "Successfully publish",
+        isSucess: true,
+        err: {},
+      });
     } catch (error) {
-        console.log(error);
-        return res.status(error.statusCode).json(
-            {
-                data:{},
-                message:error.message,
-                isSucess:false,
-                err:error.explanation
-            }
-        );
+      console.log(error);
+      return res.status(error.statusCode).json({
+        data: {},
+        message: error.message,
+        isSucess: false,
+        err: error.explanation,
+      });
     }
+  }
 
-};
+  async create(req, res) {
+    try {
+      const response = await bookingService.createBooking(req.body);
+      return res.status(StatusCodes.OK).json({
+        data: response,
+        message: "Successfully Booking",
+        isSucess: true,
+        err: {},
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(error.statusCode).json({
+        data: {},
+        message: error.message,
+        isSucess: false,
+        err: error.explanation,
+      });
+    }
+  }
+}
+// const create = async(req,res) => {
+//     try {
 
-module.exports = {
-    create
-};
+//         const response = await bookingService.createBooking(req.body);
+//         return res.status(StatusCodes.OK).json(
+//             {
+//                 data:response,
+//                 message:"Successfully Booking",
+//                 isSucess:true,
+//                 err:{}
+//             }
+//         );
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(error.statusCode).json(
+//             {
+//                 data:{},
+//                 message:error.message,
+//                 isSucess:false,
+//                 err:error.explanation
+//             }
+//         );
+//     }
+
+// };
+
+module.exports = BookingController;
--- BookingService/src/routes/v1/index.js
+++ BookingService/src/routes/v1/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const {BookingController } = require("../../controllers/index");
+// const {createChannel} = require("../../utils/message-queue");
 
 const router = express.Router();
+// const channel = await createChannel();
+const bookingController = new BookingController();
 
-router.post("/bookings",BookingController.create);
-
+router.post("/bookings",bookingController.create);
+router.post("/publish",bookingController.sendMessageQueue);
 module.exports = router;
